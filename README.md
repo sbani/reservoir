b18268be07c300858a823ccbd4d73b181ac5be2e
Sufijen Bani <cloud9@sbani.net>
2015-10-14 19:58:45 +0000

    First simple version of reservoir

---
+++ README.md
@@ -0,0 +1,43 @@
+# Reservoir
+
+With reservoir you are able to bottle up your function calls in an easy way. Make sure your (or other external services) don't become overloaded by time.
+
+## Installation
+```bash
+go get github.com/sbani/reservoir
+```
+
+## Usage
+The example shows us how to print the numbers with a delay.
+```go
+import (
+    "fmt"
+    "github.com/sbani/reservoir"
+)
+
+func printInt(i int) {
+    fmt.Println(i)
+}
+
+func main() {
+    // Never more than 1 request running at a time.
+    // Wait at least 2s between each request.
+    limiter := NewReservoir(1, 2 * time.Second)
+
+    for i := 0; i < 5; i++ {
+        limiter.add(printInt, i)
+    }
+
+    fmt.Println("This is printed first")
+
+    time.Sleep(7 * 2 * time.Second)
+}
+```
+
+## Roadmap
+
+- [x] Create project
+- [ ] Add `MaxConcurrent`functionality
+- [ ] Add http server (API) functionality 
+- [ ] Implement a maximum size of queue and stategies how to deal with an overflow
+
---
+++ reservoir.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    "reflect"
+)
+
+type job struct {
+    fn interface{} // Function to call
+    args []interface{} // Arguments for the funtion call
+}
+
+type Reservoir struct {
+    MaxConcurrent int // How many requests can be running at the same time. Default: 0 (unlimited)
+    MinTime time.Duration // How long to wait after launching a request before launching another one. Default: 0ms
+    Queue []job
+}
+
+func (rv *Reservoir) add(fn interface{}, args ...interface{}) {
+    jb := job{fn, args}
+    bn.Queue = append(bn.Queue, jb)
+}
+
+func (rv *Reservoir) start() {
+    ticker := time.NewTicker(rv.MinTime)
+    quit := make(chan bool)
+
+    for {
+       select {
+        case <- ticker.C:
+            if len(rv.Queue) > 0 {
+                rv.run()
+            }
+        case <- quit:
+            ticker.Stop()
+            return
+        }
+    }
+}
+
+// Run the oldest job in list and remove it
+func (rv *Reservoir) run() {
+    job := rv.Queue[0]
+    fn := reflect.ValueOf(job.fn)
+    in := make([]reflect.Value, len(job.args))
+    for k, param := range job.args {
+        in[k] = reflect.ValueOf(param)
+    }
+    fn.Call(in)
+
+    // remove the first/oldest element
+    rv.Queue = rv.Queue[1:]
+}
+
+func NewReservoir(maxConcurrent int, minTime time.Duration) *Reservoir {
+    rv := &Reservoir{MaxConcurrent: maxConcurrent, MinTime: minTime}
+    go rv.start()
+    return rv
+}
